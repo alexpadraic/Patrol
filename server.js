19b48032a4d81936640a4ecdbe0bc8a9fbeea6ab
alexpadraic <alex.padraic@gmail.com>
2016-10-03 16:52:12 -0700

    Make code more readable

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Dependencies
 // -----------------------------------------------------
-var express         = require('express');
-var mongoose        = require('mongoose');
-var port            = process.env.PORT || 3000;
-var database        = require('./app/config');
-var morgan          = require('morgan');
-var bodyParser      = require('body-parser');
-var methodOverride  = require('method-override');
-var app             = express();
+var express        = require('express');
+var mongoose       = require('mongoose');
+var port           = process.env.PORT || 3000;
+var database       = require('./app/config');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var app            = express();
 
 // Express Configuration
 // -----------------------------------------------------
