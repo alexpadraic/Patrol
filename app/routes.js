5ed418442dfaa1569c3ad2565f599218d0193c7f
alexpadraic <alex.padraic@gmail.com>
2016-10-03 16:57:57 -0700

    Clear-out routes that won't be used

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 // Dependencies
-var mongoose        = require('mongoose');
-var User            = require('./model.js');
-
+var mongoose   = require('mongoose');
+var Crimepoint = require('./model.js');
 
 // Opens App Routes
 module.exports = function(app) {
@@ -9,39 +8,23 @@ module.exports = function(app) {
     // GET Routes
     // --------------------------------------------------------
     // Retrieve records for all users in the db
-    app.get('/users', function(req, res){
+    app.get('/crimepoints', function(req, res){
 
         // Uses Mongoose schema to run the search (empty conditions)
-        var query = User.find({});
-        query.exec(function(err, users){
+        var query = Crimepoint.find({});
+        query.exec(function(err, crimepoint){
             if(err) {
                 res.send(err);
             } else {
-                // If no errors are found, it responds with a JSON of all users
-                res.json(users);
+                // If no errors are found, it responds with a JSON of all crimepoints
+                res.json(crimepoint);
             }
         });
     });
 
     // POST Routes
     // --------------------------------------------------------
-    // Provides method for saving new users in the db
-    app.post('/users', function(req, res){
-
-        // Creates a new User based on the Mongoose schema and the post bo.dy
-        var newuser = new User(req.body);
-
-        // New User is saved in the db.
-        newuser.save(function(err){
-            if(err)
-                res.send(err);
-            else
-                // If no errors are found, it responds with a JSON of the new user
-                res.json(req.body);
-        });
-    });
-
-    // Retrieves JSON records for all users who meet a certain set of query conditions
+    // Retrieves JSON records for all crimepoints that meet a certain set of query conditions
     app.post('/query/', function(req, res){
 
         // Grab all of the query parameters from the body.
@@ -102,21 +85,6 @@ module.exports = function(app) {
             else
                 // If no errors, respond with a JSON of all users that meet the criteria
                 res.json(users);
-        });
-    });
-
-    // DELETE Routes (Dev Only)
-    // --------------------------------------------------------
-    // Delete a User off the Map based on objID
-    app.delete('/users/:objID', function(req, res){
-        var objID = req.params.objID;
-        var update = req.body;
-
-        User.findByIdAndRemove(objID, update, function(err, user){
-            if(err)
-                res.send(err);
-            else
-                res.json(req.body);
         });
     });
 };
